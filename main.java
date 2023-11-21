83d65b1e3534ef61188b0a23df5b1b17c8cea5b2
Pioo157 <saulperalta01@gmail.com>
2023-11-20 21:32:49 -0600

    Segundo commit en el proyecto02.
    -Se experimentaron algunas funciones de la clase String.

--- main.java
+++ main.java
@@ -1,6 +1,28 @@
 public class main{
 	public static void main(String args[]){
 		System.out.println("Esto funciona");
+		String a= new String("Hola como estas");
+		String b= new String(" Mi nombre es");
+		String c= new String(" Adrien");
+		System.out.println(a.charAt(1));
+		a=a.concat(b);
+		a=a.concat(c);
+		System.out.println(a);
+		endsWT(a,"es");
+		endsWT(a,c);
+		System.out.println(c.length());
+		a=a.replace("Adrien","Noir");
+		System.out.println(a);
+	}
+
+
+	public static void endsWT(String s, String b){
+		if(s.endsWith(b)==true){
+			System.out.println("Correcto");
+		}
+		else{
+			System.out.println("No");
+		}
 	}
 }
 
